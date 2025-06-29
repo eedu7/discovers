438a5fcbc93280633588474d5f60bdab8cf7407b
eedu7 <mueedahmad067@gmail.com>
2025-06-29 19:10:38 +0500

    modified the user exists and made the email parameter optional

--- backend/app/repositories/user.py
+++ backend/app/repositories/user.py
@@ -6,8 +6,8 @@ from core.repository import BaseRepository
 
 
 class UserRepository(BaseRepository[User]):
-    async def user_exists(self, *, email: EmailStr | None, username: str | None = None):
-        if not email or not username:
+    async def user_exists(self, *, email: EmailStr | None = None, username: str | None = None):
+        if not email and not username:
             raise ValueError(
                 "Missing required input: at least one of 'email' or 'username' must be"
                 " provided to check if the user exists."
