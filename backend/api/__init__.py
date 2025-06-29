d9ccdb12c0bb16e7e84d9209503002cf4ae261a3
eedu7 <mueedahmad067@gmail.com>
2025-06-29 22:54:27 +0500

    Modified the Response Logger, and added the ResponseLoggerMiddleware to the FastAPI application and also added the exception listeners

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
-from api.favicon.favicon import favicon_router
 from api.v1 import v1_router
 
 router = APIRouter()
 
-router.include_router(favicon_router)
 router.include_router(
     v1_router,
 )
--- backend/core/server.py
+++ backend/core/server.py
@@ -1,15 +1,23 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from starlette.middleware import Middleware
 
 from api import router
+from app.exceptions import CustomException
 from core.fastapi.middlewares.response_logger import ResponseLoggerMiddleware
 from core.fastapi.middlewares.sqlalchemy import SQLAlchemyMiddleware
 
 
 def init_router(app: FastAPI) -> None:
     app.include_router(router)
+
+
+def init_listeners(app: FastAPI) -> None:
+    @app.exception_handler(Exception)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(status_code=exc.code, content={"error_code": exc.error_code, "message": exc.message})
 
 
 def make_middleware() -> List[Middleware]:
@@ -23,6 +31,7 @@ def create_app() -> FastAPI:
         middleware=make_middleware(),
     )
     init_router(app)
+    init_listeners(app)
 
     return app
 
