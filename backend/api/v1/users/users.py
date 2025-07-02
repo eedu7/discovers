dd96b1f795ac5817790ddddfef68b257a2bcf75e
eedu7 <mueedahmad067@gmail.com>
2025-07-02 23:34:46 +0500

    Made the /users/ api endpoint restricted

--- backend/api/v1/users/users.py
+++ backend/api/v1/users/users.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends
 
 from app.controllers import AuthController
 from core.factory import Factory
+from core.fastapi.dependencies import AuthenticationRequired
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", dependencies=[Depends(AuthenticationRequired)])
 async def get_all(
     auth_controller: AuthController = Depends(Factory().get_auth_controller),
 ):
