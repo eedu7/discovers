e50f96159edc1d4ba5b5fd6e79faa7b7f078b45d
eedu7 <mueedahmad067@gmail.com>
2025-07-04 08:51:22 +0500

    chore: Modified the Title and Description of the Register Card

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Metadata } from "next";
 
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { LoginRegisterCardFooter } from "@/app/auth/_components/LoginRegisterCardFooter";
 import { RegisterForm } from "@/app/auth/_components/RegisterForm";
-
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 
 export const metadata: Metadata = {
     title: "Discover | Register",
@@ -16,8 +15,11 @@ function RegisterPage() {
     return (
         <div className="grid place-content-center px-2 md:px-0">
             <Card className="w-full md:w-md">
-                <CardHeader>
-                    <CardTitle>Register</CardTitle>
+                <CardHeader className="text-center">
+                    <CardTitle className="text-lg">Create Your Discover Account</CardTitle>
+                    <CardDescription className="italic">
+                        Shop your favorite products and manage orders with ease.
+                    </CardDescription>
                 </CardHeader>
                 <CardContent>
                     <RegisterForm />
