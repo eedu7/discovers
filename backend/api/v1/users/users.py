87b4d6cdf42f5cdd9885f1fbbc124aac47535125
eedu7 <mueedahmad067@gmail.com>
2025-07-08 10:58:38 +0500

    Refined the check_use_exists api endpoint

--- backend/api/v1/users/users.py
+++ backend/api/v1/users/users.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 from pydantic import EmailStr
 
 from app.controllers import AuthController, UserController
+from app.schemas.response.user import CheckUserExists
+from core.exceptions import UnprocessableEntity
 from core.factory import Factory
 from core.fastapi.dependencies import AuthenticationRequired
 
@@ -16,16 +18,12 @@ async def get_all(
     return await auth_controller.get_all()
 
 
-@router.get("/exists", status_code=status.HTTP_200_OK)
+@router.get("/exists", status_code=status.HTTP_200_OK, response_model=CheckUserExists)
 async def check_user_exists(
     username: str | None = None,
     email: EmailStr | None = None,
     user_controller: UserController = Depends(Factory().get_user_controller),
-) -> JSONResponse:
-    if email:
-        exists = await user_controller.check_user_exists(email=email)
-
-    exists = await user_controller.check_user_exists(username=username)
-    return JSONResponse(
-        status_code=status.HTTP_200_OK, content={"exists": exists, "field": "username", "value": username}
-    )
+):
+    if not username and not email:
+        raise UnprocessableEntity("Either 'username' or 'email' must be provided to check existence.")
+    return await user_controller.check_user_exists(email, username)
