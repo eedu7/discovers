a28d9ddf159e7d419b9450a494ba33df45d4c721
eedu7 <mueedahmad067@gmail.com>
2025-07-08 13:07:15 +0500

    Added the cors middleware allowing all

--- backend/core/server.py
+++ backend/core/server.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
 from core.exceptions import CustomException
@@ -40,6 +41,13 @@ def init_listeners(app: FastAPI) -> None:
 
 def make_middleware() -> List[Middleware]:
     return [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"]
+        ),
         Middleware(
             AuthenticationMiddleware,
             backend=AuthBackend(),
