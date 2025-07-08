7f3f5f8dda01dd1db98ad00e478f901b390e032b
eedu7 <mueedahmad067@gmail.com>
2025-07-08 10:58:58 +0500

    Formatted the code base

--- backend/api/v1/users/users.py
+++ backend/api/v1/users/users.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
 from pydantic import EmailStr
 
 from app.controllers import AuthController, UserController
